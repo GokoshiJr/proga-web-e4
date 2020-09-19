b8b9669af52b93b058e0c76dca139d1d75cd3cdc
GokoshiJr <julioagonzalez18@gmail.com>
2020-09-19 13:54:36 -0400

    Arreglando el main

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World express');
+// Get, para devolver cosas
+app.get('/user', (req, res) => {
+  res.json({
+    userName: 'Julio',
+    lastName: 'Gonzalez',
+  });
 });
 
-app.get('/test', (req, res) => {
-  res.send('<h1>TEST</h1>');
+// Recibir una dato y procesarlo, puede ser en una base de datos y devolver una respuesta
+app.post('/test', (req, res) => {
+  res.send('POST REQUEST RECEIVED');
 });
 
-app.get('/about', (req, res) => {
-  res.send('About me');
+// Tomar los datos del frontend y actualizarlos, o actualizar datos de una bd
+app.put('/about', (req, res) => {
+  res.send('UPDATE REQUEST RECEIVED');
 });
 
-app.get('/contact', (req, res) => {
-  res.send('Form Contact of the API');
+// Eliminar una dato del servidor y devolver una respuesta
+app.delete('/contact', (req, res) => {
+  res.send('DELETE REQUEST RECEIVED');
 });
+
+// servicio de archivos estaticos
+app.use(express.static('./'));
 
 app.listen(3000, () => {
   console.log('Server on port 3000');
